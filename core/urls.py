05e2abc21e85dac11ae334e3745d08d76426c5be
JeffalRe <jeffalvarez1996@gmail.com>
2023-02-03 19:14:39 -0500

    Creacion de Templates heredados y aplicacion Core - Taller Jefferson Alvarez

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+#creado
+from . import views
+
+urlpatterns = [
+    #crear paths
+    path('',views.home,name="home"),
+    path('about/',views.about,name="about"),
+    path('services/',views.services,name="services"),
+    path('store/',views.store,name="store"),
+    path('contact',views.contact,name="contact"),
+    path('blog/',views.blog,name="blog"),
+    path('sample/',views.sample,name="sample"),
+    
+]
